be73c04ee903ad5db08e5492e2f34ff5ed9e2b3c
amarosuarez <amaro.suarez@iesnervion.es>
2024-10-15 14:25:12 +0200

    Agregar archivos de proyecto.

---
+++ Ej01U06/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Ej01U06.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ej01U06.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index()
+        {
+            return "Hola Mundo!";
+        }
+
+        public string Auri()
+        {
+            return "Auri";
+        }
+
+        public int num()
+        {
+            return 12;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
